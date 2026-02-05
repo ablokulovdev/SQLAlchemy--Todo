7a2fbf4fa24b423155293465bab17510b2d2c7ec
ablokulov <abloqulovnodirbek4@gmail.com>
2026-02-05 15:51:42 +0500

    database

---
+++ database.py
@@ -0,0 +1,24 @@
+from sqlalchemy import(
+    create_engine,URL,MetaData
+)
+
+from config import DB_HOST,DB_NAME,DB_PASS,DB_PORT,DB_USER
+
+DATABASE_URL = URL.create(
+    "postgresql+psycopg2",
+    username = DB_USER,
+    password=DB_PASS,
+    host=DB_HOST,
+    port=DB_PORT,
+    database=DB_NAME
+) 
+
+
+metadata_obj = MetaData()
+
+engine = create_engine(DATABASE_URL)
+
+
+
+
+
